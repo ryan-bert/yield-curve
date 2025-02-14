328b4a2a33838725445b6416c49ad1c36e901cb6
ryan-bert <ryanberti66@gmail.com>
2025-02-14 12:30:49 +0200

    Load data from db

---
+++ src/main.R
@@ -0,0 +1,25 @@
+suppressMessages({
+  library(DBI)
+  library(RPostgres)
+  library(jsonlite)
+  library(dplyr)
+})
+
+# Load postgress credentials
+current_dir <- dirname(sys.frame(1)$ofile)
+credentials_path <- file.path("~/Documents/Credentials/Raspberry Pi/financial-database.json")
+credentials <- fromJSON(credentials_path)
+
+# Connect to the database
+conn <- dbConnect(
+  Postgres(),
+  dbname = credentials$dbname,
+  host = credentials$host,
+  port = as.integer(credentials$port),
+  user = credentials$user,
+  password = credentials$password
+)
+
+# Load the data from the database
+bonds_df <- dbGetQuery(conn, "SELECT * FROM bonds")
+

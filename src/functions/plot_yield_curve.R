7b9a37a11c056075c9455e910a435ff9e459cf19
ryan-bert <ryanberti66@gmail.com>
2025-02-14 15:05:31 +0200

    Syntax

--- src/functions/plot_yield_curve.R
+++ src/functions/plot_yield_curve.R
@@ -8,7 +8,7 @@ plot_yield_curve <- function(yields_df, date) {
 
   # Convert date column to Date type
   yields_df <- yields_df %>%
-    mutate(Date = as.Date(Date))
+    mutate(Date = as.Date(.data$Date))
 
   # Get previous 1-month, 2-month, 3-month dates
   previous_dates <- as.character(as.Date(date) - c(30, 60, 90))
@@ -16,8 +16,8 @@ plot_yield_curve <- function(yields_df, date) {
 
   # Filter data for the selected dates
   yields_df <- yields_df %>%
-    filter(Date %in% selected_dates) %>%
-    select(Date, Ticker, Yield)
+    filter(.data$Date %in% selected_dates) %>%
+    select(.data$Date, .data$Ticker, .data$Yield)
 
   # Define map for maturities
   ticker_mapping <- data.frame(
@@ -34,12 +34,12 @@ plot_yield_curve <- function(yields_df, date) {
   color_mapping <- c("darkblue", "steelblue3", "steelblue2", "lightblue")
   yields_df$Date <- factor(yields_df$Date, levels = rev(selected_dates))
 
-# Define grayscale color mapping (current date black, previous dates in shades of gray)
+  # Define grayscale color mapping
   color_mapping <- c("black", "gray60", "gray80", "gray100")
   yields_df$Date <- factor(yields_df$Date, levels = rev(selected_dates))
 
   # Plot
-  plot <- ggplot(yields_df, aes(x = Years_Left, y = Yield, color = Date)) +
+  plot <- ggplot(yields_df, aes(x = .data$Years_Left, y = .data$Yield, color = .data$Date)) +
     geom_line(size = 1) +
     geom_point(size = 2) +
     scale_color_manual(values = color_mapping) +
